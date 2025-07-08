f134764bd8bba4ea2268416ae05db07ec1282d48
Sujay Ghosh <141671791+sujaycontributer@users.noreply.github.com>
2025-07-08 23:48:44 +0530

    Update route.ts

--- app/api/v1/signup/route.ts
+++ app/api/v1/signup/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest){
 
-    // how get the body req.body?
+    // how get the body req.body? const body = await req.json()
     const data = await req.json();
     // send data to database
     console.log(data)
